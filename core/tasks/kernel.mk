60f49e5b6a6c6a0876e8b3e63daca3cc4d21368d
Hubertus <Hubertus.Bergmann@web.de>
2018-07-19 10:46:06 +0200

    make clang def versatile

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -263,10 +263,10 @@ ifneq ($(filter-out false,$(USE_CCACHE)),)
     # Detect if the system already has ccache installed.
 ifeq ($(TARGET_KERNEL_CLANG_COMPILE),true)
     # Only set clang-3859424 if TARGET_KERNEL_CLANG_VERSION hasn't been set by the device config
-    TARGET_KERNEL_CLANG_VERSION := clang-stable
+    TARGET_KERNEL_CLANG_VERSION ?= $(shell ls -d $(ANDROID_BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/clang-* | xargs -n 1 basename | tail -1)
     # Find the clang-* directory containing the specified version
-    KERNEL_CLANG_VERSION := clang-stable
-    TARGET_KERNEL_CLANG_PATH ?= $(ANDROID_BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/$(KERNEL_CLANG_VERSION)/bin
+    KERNEL_CLANG_VERSION ?= $(shell ls -d $(ANDROID_BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/clang-* | xargs -n 1 basename | tail -1)
+    TARGET_KERNEL_CLANG_PATH ?= $(ANDROID_BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/$(TARGET_KERNEL_CLANG_VERSION)/bin
     ifeq ($(KERNEL_ARCH),arm64)
         KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=aarch64-linux-gnu-
     else ifeq ($(KERNEL_ARCH),arm)
